d81f96535d3c65824a26b7b0c62cf99c52e1e46e
miya-tnct <ayim2612@gmail.com>
2024-08-05 12:03:03 +0900

    fix setup.bash
    to set permission

--- setup.bash
+++ setup.bash
@@ -7,5 +7,10 @@ fi
 
 wget https://raw.githubusercontent.com/karaage0703/jetson-nano-tools/master/udev_rules/55-tegraserial.rules
 sudo mv 55-tegraserial.rules /etc/udev/rules.d/
+sudo usermod -a -G tty $(whoami)
+sudo usermod -a -G dialout $(whoami)
+sudo systemctl stop nvgetty
+sudo systemctl disable nvgetty
+sudo chmod 660 /dev/ttyTHS1
+sudo chmod 660 /dev/ttyTHS2
 echo "Please reboot"
-exit 0
