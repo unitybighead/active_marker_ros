8e307ec9f71e832b37969be5d3f2beb9fb8ccb90
unitybighead <unitybighead@gmail.com>
2024-08-07 17:38:44 +0900

    add startup.bash and setup.bash

--- setup.bash
+++ setup.bash
@@ -5,12 +5,47 @@ if [ "$(whoami)" = "root" ]; then
   exit 255
 fi
 
+cd "$(dirname "$0")"
+
+# get ROBOT_ID
+echo "Enter the ROBOT_ID"
+read ROBOT_ID
+
+# Writing automatic startup and registering the service
+echo \
+"[Unit]
+Description=\"active_marker_startup\"
+After = systemd-networkd-wait-online.service
+
+[Service]
+Type=simple
+ExecStart=\"$(pwd)/startup.bash\"
+Restart=always
+Environment=\"HOME=root\"
+
+[Install]
+WantedBy=multi-user.target" \
+| sudo tee "/lib/systemd/system/active_marker.service" > /dev/null && \
+chmod 744 "$(pwd)/startup.bash" && \
+sudo systemctl enable active_marker
+
+# install apt that needed by packages
+rosdep update
+rosdep install -i --from-paths src/*
+
+# create config.txt
+echo \
+"TEAM_COLOR=yellow
+ROBOT_ID=$ROBOT_ID" > config.txt
+
+# UART settings
 wget https://raw.githubusercontent.com/karaage0703/jetson-nano-tools/master/udev_rules/55-tegraserial.rules
 sudo mv 55-tegraserial.rules /etc/udev/rules.d/
 sudo usermod -a -G tty $(whoami)
 sudo usermod -a -G dialout $(whoami)
 sudo systemctl stop nvgetty
 sudo systemctl disable nvgetty
-sudo chmod 660 /dev/ttyTHS1
-sudo chmod 660 /dev/ttyTHS2
-echo "Please reboot"
+sudo chmod 666 /dev/ttyTHS1
+sudo chmod 666 /dev/ttyTHS2
+
+exit 0
---
+++ startup.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+. install/setup.bash
+. config.txt
+# if [ $ROBOT_ID -ge 10 ]; then
+#     export ROS_DOMAIN_ID=1"$ROBOT_ID"
+# else
+#     export ROS_DOMAIN_ID=10"$ROBOT_ID"
+# fi
+# export ROS_LOCALHOST_ONLY=1
+
+ros2 run active_marker illuminance_pub __params:=config.yaml __ns:=/am"$ROBOT_ID" &
+ros2 run active_marker illuminance_sub __params:=config.yaml __ns:=/am"$ROBOT_ID" &
+ros2 run active_marker color_sub __params:=config.yaml __ns:=/am"$ROBOT_ID" &
